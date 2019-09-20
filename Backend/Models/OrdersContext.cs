7f9e8bc7b1f1ef755afafb0a4c92293532edde08
Dmitrii Komarchev <dmitrii_komarchev@epam.com>
2019-09-20 23:16:10 +0400

    Added DbContext, initial migration and initial data seed

---
+++ Backend/Models/OrdersContext.cs
@@ -0,0 +1,23 @@
+﻿using Backend.Migrations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Models
+{
+    public class OrdersContext : DbContext
+    {
+        public OrdersContext(DbContextOptions<OrdersContext> options)
+            : base(options)
+        {
+            // ReSharper disable once VirtualMemberCallInConstructor
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.InitialDataSeed();
+        }
+    }
+}
---
+++ Backend/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Qty { get; set; }
+        public decimal Price { get; set; }
+        public decimal Total { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
