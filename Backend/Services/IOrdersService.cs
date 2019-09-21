28993305def7abdaf8f952f536ab15e819a8a4d9
Dmitrii Komarchev <dmitrii_komarchev@epam.com>
2019-09-22 00:00:38 +0400

    Finishing readonly backend

---
+++ Backend/Services/IOrdersService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Backend.Models;
+
+namespace Backend.Services
+{
+    public interface IOrdersService
+    {
+        Task<Order[]> GetOrdersAsync();
+        Task<Order> GetOrderAsync(int orderId);
+        Task<Product[]> GetOrderProductsAsync(int orderId);
+        Task<Product> GetOrderProductAsync(int orderId, int productId);
+    }
+}
